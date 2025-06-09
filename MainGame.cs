479199844ad96eb5ec8f0095fe88333d9d55a91b
Will Gale <williamlgale@gmail.com>
2025-06-09 01:41:06 +0100

    refactor: Moved the player code into its own player object class

--- MainGame.cs
+++ MainGame.cs
@@ -9,23 +9,9 @@ public class MainGame : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    private Texture2D _playerTexture;
+    private float _cameraY;
 
-    private Vector2 _playerPosition;
-    private Vector2 _playerVelocity;
-    private bool _isGrounded;
-
-    private const float Gravity = 2000f;
-    private const float JumpForce = -800f;
-    private const float MoveSpeed = 350f;
-    private const float Friction = 0.90f;
-
-    private KeyboardState _currentKeyboardState;
-    private KeyboardState _previousKeyboardState;
-
-    private float _scale = 4f;
-
-    private SpriteEffects _spriteEffect = SpriteEffects.None;
+    private Player _player;
 
     public MainGame()
     {
@@ -36,7 +22,7 @@ public class MainGame : Game
 
     protected override void Initialize()
     {
-        _playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2f, _graphics.PreferredBackBufferHeight /2f);
+        _cameraY = 0;
 
         base.Initialize();
     }
@@ -45,72 +31,17 @@ public class MainGame : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        _playerTexture = Content.Load<Texture2D>("TestPlayerSprite");
+        Texture2D playerTexture = Content.Load<Texture2D>("TestPlayerSprite");
+        _player = new Player(playerTexture, Vector2.Zero);
+
     }
 
     protected override void Update(GameTime gameTime)
     {
-        var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
 
-        _previousKeyboardState = _currentKeyboardState;
-        _currentKeyboardState = Keyboard.GetState();
-
-        float horizontalMovement = 0f;
-
-        if (_currentKeyboardState.IsKeyDown(Keys.A))
-        {
-            horizontalMovement -= MoveSpeed;
-            _spriteEffect = SpriteEffects.FlipHorizontally;
-        }
-        if (_currentKeyboardState.IsKeyDown(Keys.D))
-        {
-            horizontalMovement += MoveSpeed;
-            _spriteEffect = SpriteEffects.None;
-        }
-
-        _playerVelocity.X = horizontalMovement;
-
-        _isGrounded = _playerPosition.Y + (_playerTexture.Height * _scale) / 2f >= _graphics.GraphicsDevice.Viewport.Height;
-
-        bool isJumping = _currentKeyboardState.IsKeyDown(Keys.Space) && !_previousKeyboardState.IsKeyDown(Keys.Space);
-
-        if(isJumping && _isGrounded)
-        {
-            _playerVelocity.Y = JumpForce;
-        }
-
-        if (!_isGrounded)
-        {
-            _playerVelocity.Y += Gravity * deltaTime;
-        }
-
-        _playerPosition += _playerVelocity * deltaTime;
-
-        int windowHeight = _graphics.GraphicsDevice.Viewport.Height;
-        float halfScaledHeight = (_playerTexture.Height * _scale) / 2f;
-        if (_playerPosition.Y + halfScaledHeight > windowHeight)
-        {
-            _playerPosition.Y = windowHeight - halfScaledHeight;
-
-            if (_playerVelocity.Y > 0)
-            {
-                _playerVelocity.Y = 0f;
-            }
-        }
-
-        float halfScaledWidth = (_playerTexture.Width * _scale) / 2f;
-        int windowWidth = _graphics.GraphicsDevice.Viewport.Width;
-
-        if (_playerPosition.X - halfScaledWidth < 0)
-        {
-            _playerPosition.X = halfScaledWidth;
-            _playerVelocity.X = 0;
-        }
-        if (_playerPosition.X + halfScaledWidth > windowWidth)
-        {
-            _playerPosition.X = windowWidth - halfScaledWidth;
-            _playerVelocity.X = 0;
-        }
+        _player.Update(gameTime, GraphicsDevice);
 
         base.Update(gameTime);
     }
@@ -119,21 +50,9 @@ public class MainGame : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-
-        var origin = new Vector2(_playerTexture.Width / 2f, _playerTexture.Height / 2f);
-
-        _spriteBatch.Draw(
-            _playerTexture,
-            _playerPosition,
-            null,
-            Color.White,
-            0f,
-            origin,
-            _scale,
-            _spriteEffect,
-            0f
-        );
+        var cameraTransform = Matrix.CreateTranslation(0, (int)_cameraY, 0);
+        _spriteBatch.Begin(transformMatrix: cameraTransform, samplerState: SamplerState.PointClamp);
+        _player.Draw(_spriteBatch);
 
         _spriteBatch.End();
 
---
+++ Player.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace DoodleJumpClone
+{
+    class Player
+    {
+        private Texture2D _texture;
+        public Vector2 _position;
+        private Vector2 _velocity;
+        private bool _isGrounded;
+        private SpriteEffects _spriteEffect = SpriteEffects.None;
+
+        private const float Gravity = 1200f;
+        private const float JumpForce = -600f;
+        private const float MoveSpeed = 350f;
+        private const float Scale = 4f;
+
+        private KeyboardState _currentKeyboardState;
+        private KeyboardState _previousKeyboardState;
+
+        public int ScaledWidth => (int)(_texture.Width * Scale);
+        public int ScaledHeight => (int)(_texture.Height * Scale);
+
+        public Player(Texture2D texture, Vector2 position)
+        {
+            _texture = texture;
+            _position = position;
+        }
+
+        public void Update(GameTime gameTime, GraphicsDevice graphicsDevice)
+        {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _previousKeyboardState = _currentKeyboardState;
+            _currentKeyboardState = Keyboard.GetState();
+
+            float horizontalMovement = 0f;
+            if (_currentKeyboardState.IsKeyDown(Keys.A))
+            {
+                horizontalMovement -= MoveSpeed;
+                _spriteEffect = SpriteEffects.FlipHorizontally;
+            }
+            if (_currentKeyboardState.IsKeyDown(Keys.D))
+            {
+                horizontalMovement += MoveSpeed;
+                _spriteEffect = SpriteEffects.None;
+            }
+            _velocity.X = horizontalMovement;
+
+            _isGrounded = _position.Y + (_texture.Height * Scale) / 2f >= graphicsDevice.Viewport.Height;
+
+            bool isJumping = _currentKeyboardState.IsKeyDown(Keys.Space) && !_previousKeyboardState.IsKeyDown(Keys.Space);
+
+            if (isJumping && _isGrounded)
+            {
+                _velocity.Y = JumpForce;
+            }
+
+            if (!_isGrounded)
+            {
+                _velocity.Y += Gravity * deltaTime;
+            }
+
+            _position += _velocity * deltaTime;
+
+            int windowHeight = graphicsDevice.Viewport.Height;
+            float halfScaledHeight = (_texture.Height * Scale) / 2f;
+            if (_position.Y + halfScaledHeight > windowHeight)
+            {
+                _position.Y = windowHeight - halfScaledHeight;
+
+                if (_velocity.Y > 0)
+                {
+                    _velocity.Y = 0f;
+                }
+            }
+
+            float halfScaledWidth = (_texture.Width * Scale) / 2f;
+            int windowWidth = graphicsDevice.Viewport.Width;
+
+            if (_position.X - halfScaledWidth < 0)
+            {
+                _position.X = halfScaledWidth;
+                _velocity.X = 0;
+            }
+            if (_position.X + halfScaledWidth > windowWidth)
+            {
+                _position.X = windowWidth - halfScaledWidth;
+                _velocity.X = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            var origin = new Vector2(_texture.Width / 2f, _texture.Height / 2f);
+
+            spriteBatch.Draw(
+                _texture,
+                _position,
+                null,
+                Color.White,
+                0f,
+                origin,
+                Scale,
+                _spriteEffect,
+                0f
+            );
+        }
+    }
+}
